0593d211c4edc8e8721897ec8fe75d914218bcec
Mathis Chekraoui mathis.chekraoui@hotmail.com <mathis.chekraoui@hotmail.com>
2023-06-01 17:30:54 +0200

    dashboard pokemon done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_123207) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_142650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_123207) do
     t.datetime "updated_at", null: false
     t.integer "price"
     t.string "image_url"
-    t.string "details"
+    t.text "details"
     t.index ["user_id"], name: "index_pokemons_on_user_id"
   end
 
