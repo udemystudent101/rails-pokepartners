395a8284238fe68fd20c730d9e75c10ceca84b97
Ahmed <ahmedmouhtar@ymail.com>
2023-06-01 13:58:21 +0200

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pokemons, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
