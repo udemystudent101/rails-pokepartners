c6055a1234930683352cb4870dbbbe1aafa81352
Leo Mondary <leo_24@live.fr>
2023-06-04 23:26:45 +0200

    many changes

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
+  static targets = [ "startTime", "endTime" ]
+
   connect() {
+    flatpickr(this.startTimeTarget, {
+      // Provide an id for the plugin to work
+      plugins: [new rangePlugin({ input: "#end_time"})]})
+    flatpickr(this.endTimeTarget, {})
   }
 }
